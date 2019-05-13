3dd066c3e70048ab2b9e90933b8a3d7e0733dcab
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2019-05-13 18:23:01 -0400

    split off

--- ihmm/src/lib.rs
+++ ihmm/src/lib.rs
@@ -26,7 +26,11 @@ use ndarray_linalg::error::LinalgError;
 // use ndarray_linalg::solve::{Inverse,Determinant};
 use ndarray_linalg::solveh::{InverseH,DeterminantH};
 
+use rand;
+use rand::{thread_rng,Rng};
+
 use multivariate_normal::MVN;
+use multivariate_normal::{array_mask,array_mask_axis,array_double_select,array_double_mask};
 use dirichlet::Dirichlet;
 
 pub struct MarkovNode {
@@ -44,26 +48,95 @@ struct HiddenState {
     emission_model: MVN,
 }
 
-struct IHMM {
+impl HiddenState {
+    fn new(indices:Vec<usize>,encoding:(Array<f64,Ix2>,Array<bool,Ix2>)) -> HiddenState {
+        let emission_model = MVN::estimate_against_identity(&encoding.0.view(),&encoding.1.view()).expect("Failed to estimate ");
+        HiddenState{
+            nodes:indices,
+            emission_model
+        }
+    }
+}
+
+pub struct IHMM {
     beta: f64,
     gamma: f64,
+    beta_e: f64,
     hidden_states: Vec<HiddenState>,
     nodes: Vec<MarkovNode>,
+    encoding: (Array<f64,Ix2>,Array<bool,Ix2>),
     mixture_model: Dirichlet,
+}
+
+impl IHMM {
+    fn new(nodes:Vec<MarkovNode>) -> IHMM {
+
+        let encoding = MarkovNode::encode(&nodes);
+
+        IHMM {
+            beta: 0.,
+            gamma: 0.,
+            beta_e: 0.,
+            hidden_states: vec![],
+            nodes: nodes,
+            encoding: encoding,
+            mixture_model: Dirichlet::blank(1, 1),
+        }
+    }
+
+    fn initialize(&mut self,states:usize) {
+
+        for node in &mut self.nodes {
+            if node.parent.is_some() && node.children.is_some() {
+                node.hidden_state = Some(thread_rng().gen_range(0,states));
+            }
+        }
+    }
+
+    fn nodes_by_index(&self,indices:&[usize]) -> Vec<&MarkovNode> {
+        indices.iter().map(|i| &self.nodes[*i]).collect()
+    }
+
+    fn select_encoding(&self,indices:&[usize]) -> (Array<f64,Ix2>,Array<bool,Ix2>) {
+        let (data,mask) = &self.encoding;
+        let selected_data = array_double_select(&data.view(),indices);
+        let selected_mask = array_double_select(&mask.view(),indices);
+        (selected_data,selected_mask)
+    }
+
+    fn repartition_hidden_states(&mut self) {
+        let represented_states: HashSet<usize> = self.nodes.iter().flat_map(|n| n.hidden_state).collect();
+        let mut sorted_represented_states: Vec<usize> = represented_states.into_iter().collect();
+        sorted_represented_states.sort();
+        let mut state_node_collections = vec![];
+        for (old_state,new_state) in sorted_represented_states.iter().enumerate() {
+            let state_node_indices: Vec<usize> = self.nodes.iter().filter(|n| n.hidden_state == Some(old_state)).map(|n| n.index).collect();
+            for ni in &state_node_indices {
+                self.nodes[*ni].hidden_state = Some(*new_state);
+            }
+            state_node_collections.push(state_node_indices);
+        }
+        let mut hidden_states = vec![];
+        for hidden_state_node_indices in state_node_collections {
+            let encoding = self.select_encoding(&hidden_state_node_indices);
+            hidden_states.push(HiddenState::new(hidden_state_node_indices,encoding));
+        }
+        self.hidden_states = hidden_states;
+    }
 }
 
 
 impl MarkovNode {
 
-    pub fn encode(nodes:Vec<MarkovNode>) -> Result<(Array<f64,Ix2>,Array<bool,Ix2>),std::io::Error> {
+    pub fn encode(nodes:&Vec<MarkovNode>) -> (Array<f64,Ix2>,Array<bool,Ix2>) {
         let mut features = HashSet::new();
-        for node in &nodes {
+        for node in nodes {
             for feature in &node.features {
                 features.insert(feature);
             }
         }
         if features.iter().any(|f| *f.index() > features.len() + 1) {
-            return Err(std::io::Error::new(std::io::ErrorKind::Other,"Failed to read/write all features"));
+            panic!("Not all features read correctly, mising indices");
         }
 
         let mut data: Array<f64,Ix2> = Array::zeros((nodes.len(),features.len()));
@@ -76,7 +149,7 @@ impl MarkovNode {
             }
         }
 
-        Ok((data,mask))
+        (data,mask)
     }
 
     pub fn from_stripped_vec(stripped:&Vec<StrippedNode>) -> Vec<MarkovNode> {
@@ -146,21 +219,38 @@ pub mod tree_braider_tests {
         MarkovNode::from_stripped_vec(&StrippedNode::from_location("./testing/iris_forest/").unwrap())
     }
 
+    pub fn iris_model() -> IHMM {
+        let forest = iris_forest();
+        let model = IHMM::new(forest);
+        model
+    }
+
     #[test]
     fn test_markov_import() {
         eprintln!("Test readout");
-        MarkovNode::from_stripped_vec(&StrippedNode::from_location("./testing/iris_forest/").unwrap());
+        let stripped_nodes = StrippedNode::from_location("./testing/iris_forest/").unwrap();
+        eprintln!("Stripped nodes read");
+        MarkovNode::from_stripped_vec(&stripped_nodes);
     }
 
     #[test]
     fn test_markov_encoding_iris() {
         let nodes = iris_forest();
-        let (data,mask) = MarkovNode::encode(nodes).unwrap();
+        let (data,mask) = MarkovNode::encode(&nodes);
         eprintln!("{:?}",data.dim());
         eprintln!("{:?}",mask.dim());
         assert_eq!(data.dim().1,4);
         assert_eq!(mask.dim().1,4);
         assert_eq!(data.dim(),mask.dim());
+    }
+
+    fn test_markov_partition_iris() {
+        let mut model = iris_model();
+        model.initialize(5);
+        model.repartition_hidden_states();
+        eprintln!("{:?}",model.hidden_states.len());
+        assert_eq!(model.hidden_states.len(),5);
+        panic!();
     }
 }
 
--- ihmm/src/multivariate_normal.rs
+++ ihmm/src/multivariate_normal.rs
@@ -196,19 +196,58 @@ pub fn outer_product(a:&ArrayView<f64,Ix1>,b:&ArrayView<f64,Ix1>) -> Array<f64,I
 }
 
 pub fn array_mask<T: Copy>(data:&ArrayView<T,Ix1>,mask:&ArrayView<bool,Ix1>) -> Array<T,Ix1> {
-    mask.iter().zip(data.iter()).filter(|(b,d)| **b).map(|(b,d)| *d).collect()
+    let index_mask: Vec<usize> = mask.iter().enumerate().filter_map(|(i,b)| if *b {Some(i)} else {None}).collect();
+    data.select(Axis(0),&index_mask[..])
 }
 
-pub fn array_double_mask<T: Copy + num_traits::Zero>(data:&ArrayView<T,Ix2>,mask:&ArrayView<bool,Ix1>) -> Array<T,Ix2> {
-    let d: usize = mask.mapv(|b| if b {1_u32} else {0_u32}).into_iter().sum::<u32>() as usize;
-    let mut masked: Array<T,Ix2> = Array::zeros((d,d));
-    for (i,r) in mask.iter().zip(data.axis_iter(Axis(0))).filter(|(b,r)| **b).map(|(b,r)| r).enumerate() {
-        for (j,v) in mask.iter().zip(r.iter()).filter(|(b,v)| **b).map(|(b,v)| v).enumerate() {
-            masked[[i,j]] = *v;
-        }
-    }
-    masked
+pub fn array_mask_axis<T: Copy,U: nd::RemoveAxis>(data:&ArrayView<T,U>,mask:&ArrayView<bool,Ix1>,axis:Axis) -> Array<T,U> {
+    let index_mask: Vec<usize> = mask.iter().enumerate().filter_map(|(i,b)| if *b {Some(i)} else {None}).collect();
+    data.select(axis,&index_mask[..])
 }
+
+pub fn array_double_mask<T: Copy>(data:&ArrayView<T,Ix2>,mask:&ArrayView<bool,Ix1>) -> Array<T,Ix2> {
+    let singe_masked = array_mask_axis(data, mask, Axis(0));
+    let double_masked = array_mask_axis(&singe_masked.view(), mask, Axis(1));
+    double_masked
+}
+
+pub fn array_double_select<T:Copy>(data:&ArrayView<T,Ix2>,mask:&[usize]) -> Array<T,Ix2> {
+    let single_selected = data.select(Axis(0),mask);
+    let double_selected = single_selected.select(Axis(0),mask);
+    double_selected
+}
+
+
+// pub fn array_mask_row<T: Copy + num_traits::Zero>(data:&ArrayView<T,Ix2>,mask:&ArrayView<bool,Ix1>) -> Array<T,Ix2> {
+//     let d: usize = mask.mapv(|b| if b {1_u32} else {0_u32}).into_iter().sum::<u32>() as usize;
+//     let (r,c) = data.dim();
+//     let mut masked: Array<T,Ix2> = Array::zeros((d,c));
+//     for (i,r) in mask.iter().zip(data.axis_iter(Axis(0))).filter(|(b,r)| **b).map(|(b,r)| r).enumerate() {
+//         masked.row_mut(i).assign(&data.row(i));
+//     }
+//     masked
+// }
+//
+// pub fn array_mask_col<T: Copy + num_traits::Zero>(data:&ArrayView<T,Ix2>,mask:&ArrayView<bool,Ix1>) -> Array<T,Ix2> {
+//     let d: usize = mask.mapv(|b| if b {1_u32} else {0_u32}).into_iter().sum::<u32>() as usize;
+//     let (r,c) = data.dim();
+//     let mut masked: Array<T,Ix2> = Array::zeros((r,d));
+//     for (i,c) in mask.iter().zip(data.axis_iter(Axis(1))).filter(|(b,c)| **b).map(|(b,c)| c).enumerate() {
+//         masked.column_mut(i).assign(&data.column(i));
+//     }
+//     masked
+// }
+
+// pub fn array_double_mask<T: Copy + num_traits::Zero>(data:&ArrayView<T,Ix2>,mask:&ArrayView<bool,Ix1>) -> Array<T,Ix2> {
+//     let d: usize = mask.mapv(|b| if b {1_u32} else {0_u32}).into_iter().sum::<u32>() as usize;
+//     let mut masked: Array<T,Ix2> = Array::zeros((d,d));
+//     for (i,r) in mask.iter().zip(data.axis_iter(Axis(0))).filter(|(b,r)| **b).map(|(b,r)| r).enumerate() {
+//         for (j,v) in mask.iter().zip(r.iter()).filter(|(b,v)| **b).map(|(b,v)| v).enumerate() {
+//             masked[[i,j]] = *v;
+//         }
+//     }
+//     masked
+// }
 
 
 #[cfg(test)]
@@ -219,16 +258,31 @@ mod tree_braider_tests {
     use tree_braider4::tree_braider_tests::iris_forest;
 
     #[test]
+    fn test_mvn_array_mask() {
+        let a = array![0,1,2,3,4];
+        let m = array![true,false,true,false,true];
+        assert_eq!(array_mask(&a.view(), &m.view()),array![0,2,4]);
+    }
+
+    #[test]
+    fn test_mvn_array_double_mask() {
+        let a = array![[0,1,2,3,4],[0,1,2,3,4],[0,1,2,3,4],[0,1,2,3,4],[0,1,2,3,4]];
+        let m = array![true,false,true,false,true];
+        assert_eq!(array_double_mask(&a.view(), &m.view()),array![[0,2,4],[0,2,4],[0,2,4]]);
+    }
+
+
+    #[test]
     fn test_mvn_normal_blank() {
         let nodes = iris_forest();
-        let (data,mask) = MarkovNode::encode(nodes).unwrap();
+        let (data,mask) = MarkovNode::encode(&nodes);
         let normal = MVN::identity_prior(150,4);
     }
 
     #[test]
     fn test_mvn_normal_iris() {
         let nodes = iris_forest();
-        let (data,mask) = MarkovNode::encode(nodes).unwrap();
+        let (data,mask) = MarkovNode::encode(&nodes);
         let mut normal = MVN::identity_prior(100,4);
         normal.estimate_masked(&data.view(), &mask.view());
         eprintln!("{:?}",normal.means);
@@ -241,7 +295,7 @@ mod tree_braider_tests {
     #[test]
     fn test_mvn_normal_id_prior_iris() {
         let nodes = iris_forest();
-        let (data,mask) = MarkovNode::encode(nodes).unwrap();
+        let (data,mask) = MarkovNode::encode(&nodes);
         let normal = MVN::estimate_against_identity(&data.view(), &mask.view()).unwrap();
         eprintln!("{:?}",normal.means);
         eprintln!("{:?}",normal.covariance);
@@ -253,14 +307,14 @@ mod tree_braider_tests {
     #[test]
     fn test_mvn_mask() {
         let nodes = iris_forest();
-        let (data,mask) = MarkovNode::encode(nodes).unwrap();
+        let (data,mask) = MarkovNode::encode(&nodes);
         assert_eq!(array_mask(&data.row(0),&mask.row(0)).dim(),2);
     }
 
     #[test]
     fn test_mvn_normal_likelihood_iris() {
         let nodes = iris_forest();
-        let (data,mask) = MarkovNode::encode(nodes).unwrap();
+        let (data,mask) = MarkovNode::encode(&nodes);
         let normal = MVN::estimate_against_identity(&data.view(), &mask.view()).unwrap();
         eprintln!("{:?}",data.axis_iter(Axis(0)).zip(mask.axis_iter(Axis(0))).map(|(d,m)| normal.masked_likelihood(&d, &m)).collect::<Vec<f64>>());
         panic!();
